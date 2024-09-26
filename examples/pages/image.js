6f423edd652a356ca0ac11035a2be76915e2a7a9
CyrilleTchamda <109961606+CyrilleTchamda@users.noreply.github.com>
2024-09-26 13:59:28 +0100

    Update image.js
    There is not image in old link

--- examples/pages/image.js
+++ examples/pages/image.js
@@ -9,25 +9,25 @@ export default class Image extends Component {
         <Operation type="image" noExtra />
         <div className="widget-list image-container">
           <Lazyload throttle={200} height={300}>
-            <img src="http://ww3.sinaimg.cn/mw690/62aad664jw1f2nxvya0u2j20u01hc16p.jpg" />
+            <img src="https://cdn.textstudio.com/output/sample/normal/8/4/9/4/hello-logo-73-4948.png" />
           </Lazyload>
           <Lazyload throttle={200} height={300}>
-            <img src="http://ww1.sinaimg.cn/mw690/62aad664jw1f2nxvyo52qj20u01hcqeq.jpg" />
+            <img src="https://cdn.textstudio.com/output/sample/normal/8/4/9/4/hello-logo-73-4948.png" />
           </Lazyload>
           <Lazyload throttle={200} height={300}>
-            <img src="http://ww2.sinaimg.cn/mw690/62aad664jw1f2nxvz2cj6j20u01hck1o.jpg" />
+            <img src="https://cdn.textstudio.com/output/sample/normal/8/4/9/4/hello-logo-73-4948.png" />
           </Lazyload>
           <Lazyload throttle={200} height={300}>
-            <img src="http://ww1.sinaimg.cn/mw690/62aad664jw1f2nxvzfjv6j20u01hc496.jpg" />
+            <img src="https://cdn.textstudio.com/output/sample/normal/8/4/9/4/hello-logo-73-4948.png" />
           </Lazyload>
           <Lazyload throttle={200} height={300}>
-            <img src="http://ww1.sinaimg.cn/mw690/62aad664jw1f2nxw0e1mlj20u01hcgvs.jpg" />
+            <img src="https://cdn.textstudio.com/output/sample/normal/8/4/9/4/hello-logo-73-4948.png" />
           </Lazyload>
           <Lazyload throttle={200} height={300}>
-            <img src="http://ww4.sinaimg.cn/mw690/62aad664jw1f2nxw0p95dj20u01hc7d8.jpg" />
+            <img src="https://cdn.textstudio.com/output/sample/normal/8/4/9/4/hello-logo-73-4948.png" />
           </Lazyload>
           <Lazyload throttle={200} height={300}>
-            <img src="http://ww2.sinaimg.cn/mw690/62aad664jw1f2nxw134xqj20u01hcqjg.jpg" />
+            <img src="https://cdn.textstudio.com/output/sample/normal/8/4/9/4/hello-logo-73-4948.png" />
           </Lazyload>
           <Lazyload throttle={200} height={300}>
             <img src="http://ww1.sinaimg.cn/mw690/62aad664jw1f2nxw1kcykj20u01hcn9p.jpg" />
